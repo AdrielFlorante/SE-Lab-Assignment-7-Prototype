85cbbc1cae1f3c8ff8f03bdf9578d8b4a8f2786f
AdrielFlorante <adriel.florante@neu.edu.ph>
2025-04-27 18:02:36 +0900

    Created an extra sample PDF according to the example

--- src/ProcessedDocument.java
+++ src/ProcessedDocument.java
@@ -3,18 +3,24 @@ public class ProcessedDocument {
         DocumentRegistry registry = new DocumentRegistry();
 
         Document pdf0 = registry.createPdf("annual_report_2024.pdf", "Acme Corp", 150);
+        Document text = registry.createText("meeting_notes.txt", "UTF-8", 250);
+        Document spreadsheet = registry.createSpreadsheet("sales_data_q1.xlsx", 1000, 20);
+
+        System.out.println();
+
         pdf0.open();
 
         System.out.println();
 
-        Document text = registry.createText("meeting_notes.txt", "UTF-8", 250);
         text.open();
 
         System.out.println();
 
-        Document spreadsheet = registry.createSpreadsheet("sales_data_q1.xlsx", 1000, 20);
         spreadsheet.open();
 
         System.out.println();
+
+        Document pdf1 = registry.createPdf("summary_report.pdf", "Acme Corp", 30);
+        pdf1.open();
     }
 }
